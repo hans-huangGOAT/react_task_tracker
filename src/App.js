a06e03d5de50428299cb2a1fb41e44de19a0097c
Hans <fb_hans@outlook.com>
2021-01-21 22:29:58 +0800

    Tasks

--- src/App.js
+++ src/App.js
@@ -3,13 +3,37 @@ This is needed when using classes to create component
 import React from 'react'
 */
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Feb 6th at 1:30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Food Shopping',
+      day: 'Feb 5th at 2:30pm',
+      reminder: false,
+    },
+  ])
+
   const name = 'Brad'
 
   return (
     <div className="container">
       <Header title="Task Tracker" />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
 // The convention for naming components is uppercase the first letter.
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({title}) => {
+    const onClick = () => {
+        console.log('Click')
+    }
+
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <button className='btn'>Add</button>
+            <Button color='green' text='Add' onClick={onClick} />
         </header>
     )
 }
